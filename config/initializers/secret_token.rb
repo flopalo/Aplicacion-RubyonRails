4bf8ee5d36377e23bae3b438434a6dfa9e5a1c33
Rails Developer <user@localhost.localdomain>
2012-04-14 14:00:25 +0100

    Subiendo estructura del proyecto todo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FakerFacebook::Application.config.secret_token = 'ceeff57fee5d8f9c6ed7640d0542b67b99c99ecf8419026b64c8bb809f9a9332e480226b9dbb3638b229ef4584697519eff20f3030ac620ebaa4f5377ff9b792'
